5147a3d8671903ef307ff88317f498b817f6b94a
ShahrozS <k213822@nu.edu.pk>
2024-06-22 21:35:26 +0500

    Implemented Security Blueprint

---
+++ ContactManagment/contact-backend/src/main/java/com/shahroz/contactbackend/Services/UserService.java
@@ -0,0 +1,98 @@
+package com.shahroz.contactbackend.Services;
+
+import com.shahroz.contactbackend.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.shahroz.contactbackend.Repository.Userrepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService implements UserServiceInterface{
+
+
+    @Autowired
+    private Userrepository userrepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+
+
+    public List<User> getUsers(){
+return userrepository.findAll();
+    }
+
+    public User createUser(User user){
+user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return userrepository.save(user);
+    }
+
+
+    @Override
+    public User findById(Long id) {
+        return userrepository.findById(id).orElse(null);
+    }
+
+
+
+    @Override
+    public User updateUser(Long id, User user) {
+
+
+        User existinguser = userrepository.findById(id).orElseThrow(()->new RuntimeException("Entity not found"));
+        existinguser.setPhone_number(user.getPhone_number());
+
+
+        existinguser.setFirst_name(user.getFirst_name());
+
+        existinguser.setLast_name(user.getLast_name());
+
+        existinguser.setAddress(user.getAddress());
+
+existinguser.setEmail(user.getEmail());
+
+return userrepository.save(existinguser);
+
+
+
+    }
+    private static final int MAX_RETRY_COUNT = 3;
+
+    @Transactional
+    @Override
+    public Optional<User> deleteUser(Long id) {
+        int retryCount = 0;
+
+        while (retryCount < MAX_RETRY_COUNT) {
+            try {
+                System.out.println(("Entered the try and deleting."));
+                Optional<User> user = userrepository.findById(id);
+                userrepository.deleteByID(id);
+                return user;
+            } catch (ObjectOptimisticLockingFailureException ex) {
+
+                retryCount++;
+
+            }
+        }
+
+
+
+        throw new RuntimeException("Failed to delete user after multiple retries.");
+    }
+
+
+
+    @Override
+    public User findByEmail(String email) {
+        System.out.println("EMAILL in service " + email);
+        return findById(userrepository.findIdByEmail(email));
+    }
+}
---
+++ ContactManagment/contact-backend/src/main/java/com/shahroz/contactbackend/Services/UserServiceInterface.java
@@ -0,0 +1,20 @@
+package com.shahroz.contactbackend.Services;
+
+import com.shahroz.contactbackend.Entities.User;
+
+import java.util.List;
+import java.util.Optional;
+public interface UserServiceInterface {
+
+    public User createUser(User user);
+    public User findById(Long id);
+    List<User> getUsers();
+    User updateUser(Long id, User user);
+    Optional<User> deleteUser(Long id);
+
+    User findByEmail(String email);
+
+
+
+
+}
